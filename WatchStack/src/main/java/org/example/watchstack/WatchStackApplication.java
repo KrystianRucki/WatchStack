316b30c85a327c990edab35bf7cbc246f50c0e0e
Krystian Rucki <krystian.rucki.03@outlook.com>
2025-04-06 15:26:12 +0200

    Project init

---
+++ WatchStack/src/main/java/org/example/watchstack/WatchStackApplication.java
@@ -0,0 +1,13 @@
+package org.example.watchstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatchStackApplication.class, args);
+    }
+
+}
---
+++ WatchStack/src/test/java/org/example/watchstack/WatchStackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.watchstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
